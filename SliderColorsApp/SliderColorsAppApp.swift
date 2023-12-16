fb77e6c7b48d2cfaf854fb7ea971ff05542817eb
Kirill Satalkin <baboch@mail.ru>
2023-12-16 21:17:31 +0300

    Initial Commit

---
+++ SliderColorsApp/SliderColorsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderColorsAppApp.swift
+//  SliderColorsApp
+//
+//  Created by Кирилл Саталкин on 16.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderColorsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
